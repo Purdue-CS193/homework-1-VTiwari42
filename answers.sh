0c141277f70f381ddaa2484c58c51c6b1ece4cba
Vivan Tiwari <tiwari42@data.cs.purdue.edu>
2021-09-17 12:29:46 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,41 +4,45 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/StarWars/fafsa.jpg Memes/Spongebob/
 
 # Task 4
-
+rm -rf Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/
 
 # Task 7
-
+cp Programs/Java Homework/CS180/
 
 # Task 8
-
-
+mv Programs/Count.c Programs/C/
+mv Programs/Goodbye.c Programs/C/
 # Task 9
-
-
+mv Programs/Name.py Programs/"Python 3"/
+mv Programs/Space.py Programs/"Python 3"/
 # Task 10
-
-
+cat Essay.txt
+mv Essay.txt Homework/POL141/Essay.txt
 # Task 11
-
-
+cd..
+ls
 # Task 12
-: '
+cd ~
+.vimrc
 Type your vim/nano customizations below:
+"setnumber"
+"set autoindent"
+"set textwidth=80"
 
 
 
